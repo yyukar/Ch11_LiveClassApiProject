8f5a83857da591cdd89a08740993509332fae7a0
mstgdk <mstgdk@windowslive.com>
2025-09-25 20:46:09 +0300

    first

---
+++ src/main/java/com/patika/Ch11LiveClassApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.patika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch11LiveClassApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch11LiveClassApiProjectApplication.class, args);
+	}
+
+}
